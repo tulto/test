04dfdf87001ea9dbefef43879b0a9c5257e2ad1c
Dennis Altergott <altergott.dennis1999@gmail.com>
2021-04-13 15:10:59 +0200

    Test ob Dennis ins Repo pushen kann

---
+++ src/push_pull_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Int64
+
+
+
+if __name__ == '__main__':
+    rospy.init_node('publisher_test')
+
+    pub = rospy.Publisher("/number_count", Int64, queue_size=10)
+
+    rate = rospy.Rate(0.5)
+    counter = 1
+    while not rospy.is_shutdown():
+        msg = Int64()
+        msg.data = counter
+        counter += 1
+        pub.publish(msg)
+
+        rate.sleep()
